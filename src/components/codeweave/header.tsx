473e0c537cff07838c67b20de69e47b7b974a43d
Chandan Kumar <sark9tceone@gmail.com>
2025-10-03 17:05:58 +0000

    Add more margin

--- src/components/codeweave/header.tsx
+++ src/components/codeweave/header.tsx
@@ -53,7 +53,7 @@ const NavItems = () => {
       {navItems.map(item => (
         <Link href={item.href} key={item.label} passHref>
           <DropdownMenuItem className={cn(
-            "my-1 border focus:bg-primary/20 active:bg-primary/30",
+            "my-2 border focus:bg-primary/20 active:bg-primary/30",
             pathname === item.href && "border-primary"
           )}>
             <item.icon className="mr-2 h-4 w-4" />
