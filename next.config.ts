1bb86d28a03c231dc8f21e6c7644c91b486b3343
Chandan Kumar <sark9tceone@gmail.com>
2025-10-31 15:32:50 +0000

    Id = 1095073746611-dklrdrkmq1km4kv2kddpocc2qi90fpbg.apps.googleuserconte

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'httpshttps',
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
